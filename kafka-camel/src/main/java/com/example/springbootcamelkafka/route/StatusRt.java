56450174befa3e29b87aa09a243b4ef385acb813
Denis Melnik <53971071+Samualchi@users.noreply.github.com>
2023-06-08 17:50:02 +0300

    Add files via upload

---
+++ kafka-camel/src/main/java/com/example/springbootcamelkafka/route/StatusRt.java
@@ -0,0 +1,32 @@
+package com.example.springbootcamelkafka.route;
+
+import com.example.springbootcamelkafka.gen.Response;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+
+@Component
+public class StatusRt extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        try (JaxbDataFormat jaxb = new JaxbDataFormat("com.example.springbootcamelkafka.gen")) {
+
+            from("direct:statusRt")
+                    .process(exchange -> {
+                        Response response = new Response();
+                        response.setStatus(exchange.getIn().getHeader("Status", String.class));
+                        response.setMessage(exchange.getIn().getBody(String.class));
+                        exchange.getMessage().setBody(response, Response.class);
+                    })
+                    .marshal(jaxb)
+                    .setHeader(KafkaConstants.KEY, simple("Camel"))
+                    .to("kafka:status_topic?brokers={{kafka.broker1.host}}")
+                    .to("micrometer:timer:simple.timer?action=stop");
+        }
+    }
+}
